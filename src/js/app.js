e9c561a4b6438869c4f23fc1b46d9f55ed42edb9
kuchingneko28 <kuchingneko28@gmail.com>
2022-11-08 19:52:32 +0800

    edit dikit

--- src/js/app.js
+++ src/js/app.js
@@ -10,9 +10,9 @@ const loadCSS = (path) => {
   document.getElementsByTagName("head")[0].appendChild(css);
 };
 
-// loadCSS("https://kuchingneko28.github.io/oahe-extension/src/css/index.css"); // load css from github
+loadCSS("https://kuchingneko28.github.io/oahe-extension/src/css/index.css"); // load css from github
 
-loadCSS("http://127.0.0.1:5500/src/css/index.css"); // local css from localhost
+// loadCSS("http://127.0.0.1:5500/src/css/index.css"); // local css from localhost
 
 // fetch background.json
 fetch(backgroundJson)
